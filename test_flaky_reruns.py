30752ba1a0ea8bac4bd27f27518f22b4ec9c486e
Michael Foster <m.foster@sheffield.ac.uk>
2026-01-05 09:52:53 +0000

    Switched test order

--- test_flaky_reruns.py
+++ test_flaky_reruns.py
@@ -3,10 +3,10 @@ from flaky_reruns import FlakyReruns
 
 class TestFlakyRuns:
 
+    def test_pass(self):
+        assert True
+
     def test_create_or_delete(self):
         flaky = FlakyReruns("test.txt")
         result = flaky.create_or_delete()
         assert not result
-
-    def test_pass(self):
-        assert True
